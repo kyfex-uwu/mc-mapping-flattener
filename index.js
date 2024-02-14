a04ebd85e7dc18158f6f81dc963b5d1b86889c1d
kyfex-uwu <retrogrape117@gmail.com>
2024-02-14 11:38:57 -0700

    basically fixed it
    forgot to update this lol

--- index.js
+++ index.js
@@ -6,47 +6,188 @@ const classes = [];
 const fields = [];
 const methods = [];
 
-const classRegex = /CLASS (.*\/)*(.+?) (.*\/)*(.*)/g; //check
-const fieldRegex = /FIELD (.+?) (.+?) (.*)/g;
-const methodRegex = /METHOD (.+?) (.+?) (.*)((\n	*ARG \d .*)*)/g; //dont need to worry about <init>
-const argRegex = /(?<=ARG \d ).*/g;
+const masterRegex = /(?:\n|^)(	*)(CLASS|FIELD|METHOD) (?:[^ \n]*\/)*([^ \n]+) (?:[^ \n]*\/)*([^ \n]+)(?: ([^\n]*))?(?:\n	*COMMENT.*)*((?:\n	*ARG \d [^ \n]+)*)?/g;
+//1: tabs
+//2: class/field/method
+//3: obfuscated name
+//4: deobfuscated name
+//5: type (not for class)
+//6: arguments (only for method)
 
-const classFunc = (_, _2, obfName, _3, deobfName)=>{
-	classes.push({
-		obfName, deobfName
-	});
+const tokenMaker = (tokenRegex) => {
+	switch(tokenRegex[2]){
+		case "CLASS":
+			return classFunc(...tokenRegex);
+		case "FIELD":
+			return fieldFunc(...tokenRegex);
+		case "METHOD":
+			return methodFunc(...tokenRegex);
+	}
 };
-const fieldFunc = (_, obfName, deobfName)=>{
-	fields.push({
-		obfName, deobfName
-	});
+
+const classFriendlyRegex = /(?:.+\.)*(.*)(\[])*/g;
+const typeToClass = (clazz, informal=false) => {
+	if(clazz==undefined) return;
+
+	let arrCount=0;
+	while(clazz.startsWith("[")){
+		arrCount++;
+		clazz=clazz.substring(1);
+	}
+
+	let toReturn="";
+	let isPrimitive=true;
+	switch(clazz[0]){
+		case "B":
+			toReturn=informal?"byte":"Byte";
+			break;
+		case "C":
+			toReturn=informal?"char":"Character";
+			break;
+		case "D":
+			toReturn=informal?"double":"Double";
+			break;
+		case "F":
+			toReturn=informal?"float":"Float";
+			break;
+		case "I":
+			toReturn=informal?"int":"Integer";
+			break;
+		case "J":
+			toReturn=informal?"long":"Long";
+			break;
+		case "S":
+			toReturn=informal?"short":"Short";
+			break;
+		case "Z":
+			toReturn=informal?"bool":"Boolean";
+			break;
+		case "V":
+			toReturn=informal?"void":"Void";
+			break;
+		case "L":
+			isPrimitive=false;
+			toReturn=clazz.substring(1,clazz.length-1).replaceAll("/",".").replace("$",".");
+			break;
+	}
+
+	return {
+		obfuscated: toReturn+"[]".repeat(arrCount),
+		get deobfuscated(){
+			const components = ([...toReturn.matchAll(classFriendlyRegex)][0]||[]);
+			return this.deobfuscated=isPrimitive ?
+				toReturn : (classes.find(clazz=>clazz.obfName==components[1])||{deobfName:toReturn.split(".").pop()})
+					.deobfName+"[]".repeat(arrCount);
+		}
+	};
 };
-const methodFunc = (_, obfName, deobfName, _2, args)=>{
-	methods.push({
+
+const classRegex = /CLASS (.*?) (.*)/g;
+const classFunc = (rawString, tabs, _2, obfName, deobfName) => {
+	let longs = [...rawString.matchAll(classRegex)][0];
+	return {
+		tokenType: "CLASS",
+		tabsCount: (tabs||"").length,
 		obfName, deobfName,
-		args: args?[...args.matchAll(argRegex)].map(arg=>arg[0]):[],
-	});
+		longObf: longs[1].replaceAll("/","."),
+		longDeobf: longs[2].replaceAll("/","."),
+		raw: rawString
+	};
+};
+const fieldFunc = (_, tabs, _2, obfName, deobfName, type) => {
+	return {
+		tokenType: "FIELD",
+		tabsCount: (tabs||"").length,
+		obfName, deobfName, type: typeToClass(type)
+	};
+};
+const methodFunc = (_, tabs, _2, obfName, deobfName, type, args) => {
+	return {
+		tokenType: "METHOD",
+		tabsCount: (tabs||"").length,
+		obfName, deobfName,
+		type: type?typeToClass(type.slice(type.indexOf(")")+1)):undefined,
+		args: argFunc(type, args),
+	};
 };
 
+const paramRegex = /(B|C|D|F|I|J|S|Z|V|L[^ ]*?;)/g;
+const argRegex = /	*ARG \d (.*)/g;
+const argFunc = (type, args) => {
+	if(type==undefined||args==undefined) return;
+
+	let names=[...args.matchAll(argRegex)].map(arg=>arg[1]);
+	let types=[...type.slice(1,type.indexOf(")")).matchAll(paramRegex)].map(type=>typeToClass(type[0],true));
+
+	return names.map((name,index)=>{return {name,type:types[index]}});
+};
+
+const intRegex = /.*?_(.*)/g;
+const intSuffix = (name) => {
+	return parseInt([...name.matchAll(intRegex)][0][1])
+};
+
+const classStack={
+	put: (scope, clazz) => {
+		classStack._currClass=scope;
+		classStack._stack[scope]=clazz;
+	},
+	get: (scope) => {
+		return {
+			deobfName: classStack._stack[scope-1].deobfName,
+			obfName: classStack._stack[scope-1].obfName,
+		};
+	},
+	has: (scope) => classStack._stack[scope-1]!=undefined,
+
+	_currClass: -1,
+	_stack: []
+};
 const processFile = (file)=>{
+	let tokens=[...file.matchAll(masterRegex)]
+		.map(token=>tokenMaker(token))
+		.filter(token=>token.obfName!="<init>")
+		.filter(token=>token.tokenType=="CLASS"||token.type!=undefined)
 
+	for(const token of tokens){
+		switch(token.tokenType){
+			case "CLASS":
+				classStack.put(token.tabsCount,token);
+				classes.push({
+					obfName: token.obfName,
+					deobfName: token.deobfName,
+					longObf: token.longObf,
+					longDeobf: token.longDeobf,
+
+					_token: token
+				});
+				break;
+			case "FIELD":
+				if(classStack.has(token.tabsCount))
+					fields.push({
+						obfName: token.obfName,
+						deobfName: token.deobfName,
+						class: classStack.get(token.tabsCount),
+						type: token.type,
+
+					_token: token
+					});
+				break;
+			case "METHOD":
+				if(classStack.has(token.tabsCount))
+					methods.push({
+						obfName: token.obfName,
+						deobfName: token.deobfName,
+						args: token.args,
+						class: classStack.get(token.tabsCount),
+						type: token.type,
+
+					_token: token
+					});
+				break;
+		}
+	}
 };
-
-const javaTemplate=[
-	"package com.kyfexuwu.jsonblocks;\n\n"+
-
-	"/* This file was automatically generated, please do\n"+
-	" * not mess with it unless you know what you're doing\n"+
-	" */\n\n"+
-
-	"import java.util.HashMap;\n\n"+
-
-	"public class Translations {\n"+
-	"    public static HashMap<String, String> translations = new HashMap<>(){\n",
-
-	"    };\n"+
-	"}\n"
-];
 
 glob.glob('./mappings/**/*.mapping')
 	.then(arr=>{
@@ -61,24 +202,51 @@ glob.glob('./mappings/**/*.mapping')
 
 		for(const fileName of arr){
 			const file = fs.readFileSync("./"+fileName).toString()
+
 			processFile(file);
+			classStack._stack=[];
+			classStack._currClass=-1;
 
 			counter++;
 			bar.tick();
 		}
+
+		fs.rmSync("./out", { recursive: true, force: true });
+		fs.mkdirSync("./out");
 	}).then(_=>{
-		let toExport = javaTemplate[0];
+		let classesSorted=[];
+		for(const clazz of classes){
+			classesSorted[intSuffix(clazz.obfName)]=`${clazz.obfName} ${clazz.deobfName} ${clazz.longObf} ${clazz.longDeobf}`;
+		}
 
-		for(const clazz of classes)
-			toExport+=`		{"${clazz.deobfName}", "${clazz.obfName}"},\n`;
-		for(const method of methods)
-			toExport+=`		{"${method.deobfName}", "${method.obfName}"},\n`;
-		for(const field of fields)
-			toExport+=`		{"${field.deobfName}", "${field.obfName}"},\n`;
+		fs.writeFileSync("./out/classes.txt",classesSorted.join("\n")+"\n");
 
-		toExport+=javaTemplate[1];
+	}).then(_=>{
+		let fieldsSorted=[];
+		for(const field of fields.filter(field=>field.obfName.startsWith("field"))){
+			fieldsSorted[intSuffix(field.obfName)]=`${field.obfName} ${field.deobfName} ${field.type.deobfuscated}`;
+		}
 
-		fs.writeFileSync("./Translations.java",toExport);
+		fs.writeFileSync("./out/fields.txt",fieldsSorted.join("\n")+"\n");
+
+		let compFieldsSorted=[];
+		for(const comp of fields.filter(field=>field.obfName.startsWith("comp"))){
+			compFieldsSorted[intSuffix(comp.obfName)]=`${comp.obfName} ${comp.deobfName} ${comp.type.deobfuscated}`;
+		}
+
+		fs.writeFileSync("./out/compfields.txt",compFieldsSorted.join("\n")+"\n");
+	}).then(_=>{
+		let methodsSorted=[];
+		for(const method of methods.filter(method=>method.obfName.startsWith("method"))){
+			methodsSorted[intSuffix(method.obfName)]=`${method.obfName} ${method.deobfName} ${method.type.deobfuscated} ${
+					(method.args||[]).map(arg=>arg.name+":"+arg.type.deobfuscated+" ").join("")
+				}`;
+		}
+
+		fs.writeFileSync("./out/methods.txt",methodsSorted.join("\n")+"\n");
+		
+	}).then(_=>{
+		commandLineHandler("help").then(msg=>console.log("mc-mapping-flattener\n"+msg));
 	});
 
 //--
@@ -101,18 +269,19 @@ async function commandLineHandler(...input) {
         "lookup [class|field|method] (obfuscated name|deobfuscated name)"
         "";
     case "lookup":
+    	let regex = new RegExp(input[2]);
     	switch (input[1]){
     		case "class":
-    			return classes.find(clazz=>
-    				clazz.obfName==input[2] || clazz.deobfName==input[2]
+    			return classes.filter(clazz=>
+    				clazz.obfName.match(regex) || clazz.deobfName.match(regex)
     			);
     		case "field":
-    			return fields.find(field=>
-    				field.obfName==input[2] || field.deobfName==input[2]
+    			return fields.filter(field=>
+    				field.obfName.match(regex) || field.deobfName.match(regex)
     			);
     		case "method":
-    			return methods.find(method=>
-    				method.obfName==input[2] || method.deobfName==input[2]
+    			return methods.filter(method=>
+    				method.obfName.match(regex) || method.deobfName.match(regex)
     			);
     		default:
     			return "must be class, field, or method";
